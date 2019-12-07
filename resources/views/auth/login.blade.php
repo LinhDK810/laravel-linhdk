a017b422824ab9a31dcc3c47670b7b2fd5f91238
unknown <Dng Khanh Linh>
2019-12-07 16:44:33 +0700

    hihi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,12 +17,7 @@
                             <div class="col-md-6">
                               {{--<input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="name" value="{{ old('email') }}" required autocomplete="email" autofocus>--}}
                                 <input id="name" type="text" class="form-control" name="name" value="{{old('name')}}" required>
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+
                         </div>
 
                         <div class="form-group row">
